e1493b69d373948508f07d88df3cb9b9eb5e21d6
Ryan Sandor Richards <sandor.richards@gmail.com>
2010-08-31 00:13:24 -0700

    Fleshed out the project a bit more and setup some stub classes and modules.

--- server.js
+++ server.js
@@ -1,46 +1,17 @@
 /*
   server.js
-  Project: nodegs
-  Ryan Sandor Richards
+  http://www.github.com/rsandor/nodegs
+  By Ryan Sandor Richards
   MIT License Copyright 2010 Ryan Sandor Richards, All Rights Reserved
 */
 
-var http = require("http");
-var sys = require("sys");
-var io = require("socket.io");
+var sys = require('sys')
+  , nodegs = require('./lib/nodegs')
 
-//
-// Create the http server
-//
-var server = http.createServer(function(req, res){
-  res.writeHead(200, {"Content-Type":"text/plain"});
-  res.end('{}');
-})
-server.listen(8080);
+// Check for server version information flag
+if (process.argv.length > 1 && process.argv[1] == '--version') {
+  sys.puts('nodegs version ' + nodegs.version);
+}
 
-//
-// Create the websocket interface
-//
-var socket = io.listen(server);
-socket.on('connection', function(client){
-  client.on('message', function(msg){
-    sys.log("Message Recieved: " + msg);
-    
-    // Simple reverser to test server -> client communications
-    var ret = "";
-    for (var i = msg.length - 1; i >= 0; i--) {
-      ret += msg.charAt(i);
-    }
-    
-    client.send(ret);
-  })
-
-  client.on('disconnect', function(){
-    sys.log("Client disconnected");
-  })
-
-  process.addListener("SIGINT", function(){
-    sys.log("SERVER CLOSING")
-    setTimeout(function(){process.exit(0)}, 3000);
-  })
-});
+// Initialize the game server
+nodegs.Server.init(8080);
