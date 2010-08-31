d276d6736fc84771552224d92ced6d0b8a3eb8c5
Ryan Sandor Richards <sandor.richards@gmail.com>
2010-08-31 03:25:57 -0700

    Worked out game event routing between server components.

--- server.js
+++ server.js
@@ -9,9 +9,26 @@ var sys = require('sys')
   , nodegs = require('./lib/nodegs');
 
 // Check for server version information flag
-if (process.argv.length > 1 && process.argv[1] == '--version') {
-  sys.puts('nodegs version ' + nodegs.version);
+if (process.argv.length > 1 && process.argv[2] == '--version') {
+  sys.puts(nodegs.info.version);
 }
-
-// Initialize the game server
-var gameServer = new nodegs.GameServer(8080);
+// Check for help page
+else if (process.argv.length > 1 && process.argv[2] == '--help' || process.argv[2] == '--info') {
+  var info = nodegs.info;
+  var msg = info.name + " version " + info.version + 
+    "\n" + info.copyright + " " + info.author + 
+    "\nWebsite: " + info.website;
+  sys.puts(msg);
+}
+// Otherwise start the game server
+else {
+  // Initialize the game server
+  var server = new nodegs.GameServer();
+  var broadcaster = new nodegs.Broadcaster();
+  
+  // Have the broadcaster listen for incoming events 
+  server.on('event', broadcaster.route);
+  
+  // Begin listening for incoming game events
+  server.listen(8080);
+}
